a95206afd98a24a8ba2469a00291914add7de578
ELai97k <99068391+ELai97k@users.noreply.github.com>
2022-10-02 16:54:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,59 @@ from discord.ext import commands
 intents = discord.Intents.default().all()
 intents.members = True
 
-client = commands.Bot(command_prefix="+", case_insensitive=True, intents=intents)
+# custom help command
+class CustomHelpCommand(commands.HelpCommand):
+    # cogs and commands
+    async def send_bot_help(self, mapping):
+        command_prefix = "+"
+        embed = discord.Embed(title=f"{client.user.name}'s Cogs & Commands", color=0xffc90d)
+        description = self.context.bot.description
+        if description:
+            embed.description = description
+
+        for cog, commands in mapping.items():
+            if cog is not None:
+                name = cog.qualified_name
+                filtered = await self.filter_commands(commands, sort=True)
+
+                if filtered:
+                    value = '\u2002 '.join('`' + c.name + '`' for c in commands if not c.hidden)
+
+                    if cog and cog.description:
+                        value = '{0}\n{1}'.format(cog.description, value)
+
+                    embed.add_field(name=name, value=value, inline=True)
+                    embed.set_footer(text=f"Use {command_prefix}help [cog] or {command_prefix}help [command] for more info.")
+
+        await self.get_destination().send(embed=embed)
+
+    # cog info
+    async def send_cog_help(self, cog):
+        command_prefix = "+"
+        embed = discord.Embed (
+            title = f"{cog.qualified_name} Commands",
+            description = f"{cog.description}\n```{[command.name for command in cog.get_commands()]}```",
+            color=0xffc90d
+        )
+        embed.set_footer(text=f"Use {command_prefix}help [command] for more info on a command.")
+        await self.get_destination().send(embed=embed)
+
+    # command info
+    async def send_command_help(self, command):
+        embed = discord.Embed (
+            color=0xffc90d
+        )
+        embed.add_field (
+            name=f"{command.name}",
+            value=command.help,
+            inline=False
+        )
+        await self.get_destination().send(embed=embed)
+
+client = commands.Bot(command_prefix="+", 
+help_command=CustomHelpCommand(), 
+case_insensitive=True, 
+intents=intents)
 
 # cogs
 for filename in os.listdir("./cogs"):
